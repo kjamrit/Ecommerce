b871401e8b3702b7006c51bfb5676e7c98d0ee5a
kjamrit <f20211696@pilani.bits-pilani.ac.in>
2024-05-05 08:05:00 +0530

    Added CRUD operations

--- ecomapp/models.py
+++ ecomapp/models.py
@@ -10,7 +10,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
 
 class Order(models.Model):

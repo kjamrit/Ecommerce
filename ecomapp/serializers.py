667d618f4e57387a5b4110f8a6278f09b88f6a56
kjamrit <f20211696@pilani.bits-pilani.ac.in>
2024-05-05 21:38:51 +0530

    Setup and CRUD operations

--- ecomapp/serializers.py
+++ ecomapp/serializers.py
@@ -2,13 +2,6 @@ from rest_framework import serializers
 from .models import Customer, Product, Order, User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        # Add custom claims if needed
-        return token
-
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -20,6 +13,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        return token
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
